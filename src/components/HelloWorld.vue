d59332ab3f5e371c2446bc7c374462e25753cc26
Paulo <paulonrss@gmail.com>
2022-08-18 00:57:09 -0300

    Hello World feito, porem codigo não está legal, precisa de melhorias no futuro, muita cara de gambiarra

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,20 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- <div class="frente">
+      Hello
+    </div> -->
+    <div class="div1" :style="{width: porcentagem1}">
+      Hello World
+    </div>
+    <div class="div2" :style="{width: porcentagem2}">
+      Hello World
+    </div>
+    <div class="div3" :style="{width: porcentagem3}">
+      Hello World
+    </div>
+
+  <!-- <div class="teste" :style="{width: porcentagem}">A</div> -->
+
   </div>
 </template>
 
@@ -38,24 +23,83 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      porcentagem1: 0,
+      porcentagem2: 0,
+      porcentagem3: 0
+    }
+  },
+  created() {
+    this.div1()
+  },
+  methods:{
+    div1(){
+      let porcentagemAtual = 0
+      let intPorcetagem = 60
+      // let finalizou1 = false
+      // let finalizou2 = false
+        setInterval(()=> {
+          if(porcentagemAtual > intPorcetagem) {
+            if(porcentagemAtual > intPorcetagem * 2){
+              if(porcentagemAtual > intPorcetagem * 3){
+                porcentagemAtual = 0
+                return
+              } 
+              this.porcentagem3 = `${porcentagemAtual - (intPorcetagem * 2)}%`
+            }else{
+              this.porcentagem2 = `${porcentagemAtual - intPorcetagem}%`
+            }
+          }else{
+            this.porcentagem1 = `${porcentagemAtual}%`
+          }
+          porcentagemAtual++
+        }, 50)
+      // this.div2()
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.hello{
+  font-size: 20em;
+  background-color: rgb(66, 65, 65);
+  margin-left: 120px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.tras{
+  width: 100%;
+  color: white;
+  // background-color: black;
+  text-align: left;
+  margin-left: 100px;
+  position: absolute;
+  -webkit-transform: skew(-20deg);
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.div1, .div2, .div3{
+  text-align: left;
+  margin-left: 100px;
+  position: absolute;
+  -webkit-transform: skew(-20deg);
 }
-a {
-  color: #42b983;
+.div1{
+  // z-index: -3;
+  color: rgb(0, 0, 0);
+  background-color: rgb(0, 0, 0);;
 }
+.div2{
+  // z-index: -2;
+  color: black;
+  background-color: rgb(255, 255, 255);;
+}
+.div3{
+  // z-index: -1;
+  color: black;
+  background-color: rgb(0, 0, 0);;
+}
+
 </style>
